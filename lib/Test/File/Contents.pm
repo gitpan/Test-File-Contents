acdaa23f29230d4d86833b5754dd66427cc81c48
David E. Wheeler <david@justatheory.com>
2011-02-18 21:08:31 +0000

    Import of DWHEELER/Test-File-Contents-0.10 from CPAN.
    gitpan-cpan-distribution: Test-File-Contents
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         DWHEELER/Test-File-Contents-0.10.tar.gz
    gitpan-cpan-author:       DWHEELER
    gitpan-cpan-maturity:     released

--- lib/Test/File/Contents.pm
+++ lib/Test/File/Contents.pm
@@ -3,137 +3,154 @@ package Test::File::Contents;
 use warnings;
 use strict;
 
-=head1 NAME
+=head1 Name
 
 Test::File::Contents - Test routines for examining the contents of files
 
-=head1 VERSION
-
-Version 0.05
-
 =cut
 
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-$VERSION = '0.05';
-
-use Exporter;                   # load the class
-@ISA         = qw(Exporter);    # set it as the base class
-@EXPORT      = qw(file_contents_is file_contents_isnt file_contents_like file_contents_unlike file_md5sum file_contents_identical);
-@EXPORT_OK   = qw();            # no other optional functions
-%EXPORT_TAGS = qw();            # no groups of functions
+our $VERSION = '0.10';
 
 use Test::Builder;
 use Digest::MD5;
 use File::Spec;
-my $Test = Test::Builder->new();
 
-=head1 SYNOPSIS
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
+    file_contents_eq
+    file_contents_ne
+    file_contents_is
+    file_contents_isnt
+    file_contents_like
+    file_contents_unlike
+    file_md5sum
+    file_contents_identical
+);
 
-    use Test::File::Contents;
+my $Test = Test::Builder->new;
 
-    file_contents_is       ($file,  $string,  $test_description);
-    file_contents_like     ($file,  qr/foo/,  $test_description);
-    file_md5sum            ($file,  $md5sum,  $test_description);
-    file_contents_identical($file1, $file2,   $test_description);
+=head1 Synopsis
 
+  use Test::File::Contents;
 
-=head1 FUNCTIONS
+  file_contents_eq        $file,  $string,  $description;
+  file_contents_like      $file,  qr/foo/,  $description;
+  file_md5sum             $file,  $md5sum,  $description;
+  file_contents_identical $file1, $file2,   $description;
 
-=head2 file_contents_is
+=head1 Description
 
-Checks for an exact match on the file's contents. Pass in a Unix-style file
-name and it will be converted for the local file system.
+Got an app that generates files? Then you need to test those files to make
+sure that their contents are correct. This module makes that easy. Use its
+test functions to make sure that the contents of files are exactly what you
+expect them to be.
 
-Note: performs a dumb "eq" comparison, sucking the whole file into memory.
+=head1 Interface
 
-Also note: I am aware of the grammatical confusion in the function name
-- got a better suggestion?
+=head2 Test Functions
+
+=head3 file_contents_eq
+
+  file_contents_eq $file, $string, $description;
+
+Checks that the file's contents are equal to a string. Pass in a Unix-style
+file name and it will be converted for the local file system. The old name for
+this function, C<file_contents_is>, remains as an alias.
 
 =cut
 
-sub file_contents_is {
+sub file_contents_eq($$;$) {
     my ($file, $string, $desc) = @_;
     return _compare(
         $file,
         sub { shift eq $string },
-        $desc || 'file contents match string',
-        "File $file does not match '$string'",
+        $desc || "$file contents equal to string",
+        "File $file contents not equal to '$string'",
     );
 }
 
-=head2 file_contents_isnt
+*file_contents_is = \&file_contents_eq;
 
-Checks that the file's contents do not match a string. Pass in a Unix-style file
-name and it will be converted for the local file system.
+=head3 file_contents_ne
 
-Note: performs a dumb "ne" comparison, sucking the whole file into memory.
+  file_contents_ne $file, $string, $description;
 
-Also note: I am aware of the grammatical confusion in the function name
-- got a better suggestion?
+Checks that the file's contents do not equal a string. Pass in a Unix-style
+file name and it will be converted for the local file system. The old name for
+this function, C<file_contents_isnt>, remains as an alias.
 
 =cut
 
-sub file_contents_isnt {
+sub file_contents_ne($$;$) {
     my ($file, $string, $desc) = @_;
     return _compare(
         $file,
         sub { shift ne $string },
-        $desc || 'file contents do not match string',
-        "File $file matches '$string'",
+        $desc || "$file contents not equal to string",
+        "File $file contents equal to '$string'",
     );
 }
 
-=head2 file_contents_like
+*file_contents_isnt = \&file_contents_ne;
 
-Checks for a regexp match against the file's contents. You must provide it
-with a qr// style regexp. Pass in a Unix-style file name and it will be
-converted for the local file system.
+=head3 file_contents_like
 
+  file_contents_like $file, qr/foo/, $description;
+
+Checks that the contents of a file match a regular expression. The regular
+expression must be passed as a regular expression object created by C<qr//>.
 
 =cut
 
-sub file_contents_like {
-    my ($file, $regexp, $desc) = @_;
+sub file_contents_like($$;$) {
+    my ($file, $regex, $desc) = @_;
     return _compare(
         $file,
-        sub { shift =~ /$regexp/ },
-        $desc || 'file contents match regexp',
-        "File $file does not match '$regexp'",
+        sub { shift =~ /$regex/ },
+        $desc || "$file contents match regex",
+        "File $file contents do not match /$regex/",
     );
 }
 
-=head2 file_contents_unlike
+=head3 file_contents_unlike
 
-Checks for a regexp failed match against the file's contents. You must provide
-it with a qr// style regexp. Pass in a Unix-style file name and it will be
-converted for the local file system.
+  file_contents_unlike $file, qr/foo/, $description;
+
+Checks that the contents of a file I<do not> match a regular expression. The
+regular expression must be passed as a regular expression object created by
+C<qr//>.
 
 =cut
 
-sub file_contents_unlike {
-    my ($file, $regexp, $desc) = @_;
+sub file_contents_unlike($$;$) {
+    my ($file, $regex, $desc) = @_;
     return _compare(
         $file,
-        sub { shift !~ /$regexp/ },
-        $desc || 'file contents do not match regexp',
-        "File $file matches '$regexp'",
+        sub { shift !~ /$regex/ },
+        $desc || "$file contents do not match regex",
+        "File $file contents match /$regex/",
     );
 }
 
-=head2 file_md5sum
+=head3 file_md5sum
 
-Checks whether a file matches a given md5sum. The md5sum should be provided as
-a hex string, eg. "6df23dc03f9b54cc38a0fc1483df6e21". Pass in a Unix-style
-file name and it will be converted for the local file system.
+  file_md5sum $file, $md5sum, $description;
+
+Checks whether a file matches a given MD5 checksum. The checksum should be
+provided as a hex string, for example, C<6df23dc03f9b54cc38a0fc1483df6e21>.
+Pass in a Unix-style file name and it will be converted for the local file
+system.
 
 =cut
 
-sub file_md5sum {
-    my $file = $_[0] =~ m{/}
-        ? File::Spec->catfile(split m{/}, shift)
-        : shift;
+sub file_md5sum($$;$) {
+    my $arg_file = shift;
+    my $file = $arg_file =~ m{/}
+        ? File::Spec->catfile(split m{/}, $arg_file)
+        : $arg_file;
     my ($md5sum, $desc) = @_;
-    $desc ||= "file matches md5sum";
+    $desc ||= "$arg_file has md5sum";
     local *IN;
     if (open IN, $file) {
         my $ctx = undef;
@@ -144,33 +161,32 @@ sub file_md5sum {
             $Test->ok(1, $desc);
             return 1;
         } else {
-            $Test->diag("File $file has md5sum " . $result . " not $md5sum");
             $Test->ok(0, $desc);
+            $Test->diag("    File $arg_file has md5sum $result, not $md5sum");
             return 0;
         }
         close IN;
     } else {
-        $Test->diag("Could not open file $file: $!");
         $Test->ok(0, $desc);
+        $Test->diag("    Could not open file $file: $!");
         return 0;
     }
 }
 
-=head2 file_contents_identical
+=head3 file_contents_identical
+
+  file_contents_identical $file1, $file2, $description;
 
 Tests that the contents of two files are identical. Pass in a Unix-style file
 name and it will be converted for the local file system.
 
 =cut
 
-sub file_contents_identical {
-    my $file1 = $_[0] =~ m{/}
-        ? File::Spec->catfile(split m{/}, shift)
-        : shift;
-    my $file2 = $_[0] =~ m{/}
-        ? File::Spec->catfile(split m{/}, shift)
-        : shift;
-    my $desc = shift || 'file contents identical';
+sub file_contents_identical($$;$) {
+    my ($f1, $f2) = (shift, shift);
+    my $file1 = $f1 =~ m{/} ? File::Spec->catfile(split m{/}, $f1) : $f1;
+    my $file2 = $f2 =~ m{/} ? File::Spec->catfile(split m{/}, $f2) : $f2;
+    my $desc = shift || "$f1 and $f2 contents identical";
 
     my $ok;
     local(*IN1, *IN2);
@@ -183,18 +199,18 @@ sub file_contents_identical {
                 $Test->ok(1, $desc);
                 return 1;
             } else {
-                $Test->diag("Files $file1 and $file2 are not identical.");
                 $Test->ok(0, $desc);
+                $Test->diag("    Files $f1 and $f2 are not identical.");
                 return 0;
             }
         } else {
-            $Test->diag("Could not open file $file1: $!");
             $Test->ok(0, $desc);
+            $Test->diag("    Could not open file $file1: $!");
             return 0;
         }
     } else {
-        $Test->diag("Could not open file $file2: $!");
         $Test->ok(0, $desc);
+        $Test->diag("    Could not open file $file2: $!");
         return 0;
     }
 }
@@ -213,39 +229,43 @@ sub _compare {
             $Test->ok(1, $desc);
             return 1;
         } else {
-            $Test->diag($err);
             $Test->ok(0, $desc);
+            $Test->diag("    $err");
             return 0;
         }
     } else {
-        $Test->diag("Could not open file $file: $!");
         $Test->ok(0, $desc);
+        $Test->diag("    Could not open file $file: $!");
         return 0;
     }
 }
 
-=head1 AUTHOR
+1;
 
-Kirrily Robert, C<< <skud@cpan.org> >>
+=head1 Authors
 
-Contributors:
+=over
 
-David Wheeler, C<< <dwheeler@cpan.org> >>
+=item * Kirrily Robert <skud@cpan.org>
 
-=head1 BUGS
+=item * David E. Wheeler <david@kineticode.com>
 
-Please report any bugs or feature requests to
-C<bug-test-file-contents@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.  I will be notified, and then you'll automatically
-be notified of progress on your bug as I make changes.
+=back
 
-=head1 COPYRIGHT & LICENSE
+=head1 Support
 
-Copyright 2004-2005 Kirrily Robert.
+This module is stored in an open L<GitHub
+repository|http://github.com/theory/test-file-contents/tree/>. Feel free to
+fork and contribute!
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+Please file bug reports via L<GitHub
+Issues|http://github.com/theory/test-file-contents/issues/> or by sending mail to
+L<bug-Test-File-Contents@rt.cpan.org|mailto:bug-Test-File-Contents@rt.cpan.org>.
 
-=cut
+=head1 Copyright and License
 
-1; # End of Test::File::Contents
+Copyright (c) 2004-2007 Kirrily Robert. Some Rights Reserved.
+Copyright (c) 2007-2011 David E. Wheeler. Some Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
